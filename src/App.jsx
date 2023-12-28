c23f8c3dce0beceae937d1429bae6237564b6fc3
Grace Chen Abudi <cheezumcali23@gmail.com>
2023-12-28 18:22:18 +0200

    chore: passing custom arguments to event functions

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,9 @@ import CoreConcept from "./components/CoreConcept.jsx";
 import TabButton from "./components/TabButton.jsx";
 
 function App() {
-  function handleClick() {
-    console.log("Aloha Everyone! - clicked");
+  function handleClick(clickedButton) {
+    // clickedButton => 'components', 'jsx', 'props', 'state'
+    console.log(clickedButton);
   }
 
   return (
@@ -24,10 +25,12 @@ function App() {
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton onClick={handleClick}>Components</TabButton>
-            <TabButton onClick={handleClick}>JSX</TabButton>
-            <TabButton onClick={handleClick}>Props</TabButton>
-            <TabButton onClick={handleClick}>State</TabButton>
+            <TabButton onClick={() => handleClick("components")}>
+              Components
+            </TabButton>
+            <TabButton onClick={() => handleClick("jsx")}>JSX</TabButton>
+            <TabButton onClick={() => handleClick("props")}>Props</TabButton>
+            <TabButton onClick={() => handleClick("state")}>State</TabButton>
           </menu>
           Dynamic Content
         </section>
